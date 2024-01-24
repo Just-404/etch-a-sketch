e86e8f25325e35d4d2bfa9143cbd4301283cc36b
Eddy <154628904+Just-404@users.noreply.github.com>
2024-01-24 15:17:19 -0400

    Add button that change grid size
    Created a function that deletes the previous size and an event listener
    on the button that allows the user to choose the size of the grid
    between 1-100. Length validation was also added.

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const container = document.querySelector('.container')
+const btn = document.querySelector('button');
 
 function createColumn(len){
     
-        const columnDiv = document.createElement('div');
-        columnDiv.classList.add('column');
-        columnDiv.setAttribute('style','flex: 1; display:flex; flex-direction: column');
-        columnDiv.style.border = '2px solid gray';
-        
-        container.appendChild(columnDiv);
+      const columnDiv = document.createElement('div');
+      columnDiv.classList.add('column');
+      columnDiv.setAttribute('style','flex: 1; display:flex; flex-direction: column');
+      columnDiv.style.border = '2px solid gray';
+      
+      container.appendChild(columnDiv);
+      createRow(columnDiv, len);
 
-        createRow(columnDiv, len);
 }
 
 function createRow(columnDiv, len){
@@ -38,8 +39,30 @@ function createGrid(len){
 function getRandomNumber(){
   return Math.floor(Math.random() * 255);
 }
+
+function deleteGrid() {
+  const colDiv = document.querySelectorAll('.column');
+  colDiv.forEach(element => {
+    element.remove();
+  });
+
+}
 let len = 16;
 createGrid(len);
+
+btn.addEventListener('click', () => {
+   
+  len = prompt('Enter the grid dimension: ', 'Choose a number between 1-100');
+
+  if(len <= 100 && len >= 1){
+    deleteGrid();
+    createGrid(len);
+  } 
+  else if(len == "" || len == null) alert('Invalid syntax. Try again!');
+  else alert('The number must be between 1 and 100!');
+
+  
+});
 
 
 
